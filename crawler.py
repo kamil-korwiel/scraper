8dd4c8916df290c0a0832da6dbd2deeebbeb5350
kamil-korwiel <kamilkorwiel@gmail.com>
2024-03-24 22:16:05 +0100

    Created some notes in order to implement dfs and bfs

--- crawler.py
+++ crawler.py
@@ -5,52 +5,47 @@ from lxml import html
 from lxml.html import HtmlElement
 from urllib.parse import urljoin, urlsplit
 
+from queue import Queue , LifoQueue
+
+
+def get_html(url:str) -> str:
+    response: Response = requests.get(url)
+    if response.status_code == 200:
+        return response.content
+
+
+def get_links(current_url:str):
+    text_html = get_html(current_url)
+    tree: HtmlElement = html.fromstring(text_html)
+    expresion = "//body//a"
+    elements: list[HtmlElement] = tree.xpath(expresion)
+    
+    def normalise (url:str):
+        parse = urlsplit(url)
+        base_url = f"{parse.scheme}://{parse.netloc}"
+        normalise_links = [urljoin(base_url, e.attrib['href'])  for e in elements ]
+        return normalise_links
+
+    return normalise(current_url)    
+
+def dfs(url: str, deep_limit: int):
+    pass
+
+def bfs(url: str, deep_limit: int):
+    pass
+
 
 class Crawler:
     url: str = ""
-    deep: int = 0
+    deep_limit: int = 2
     grap_url: dict[str : list[str]]
-    all_urls: set[str] = {}
+    visited: set[str] = {}
 
-    def __init__(self, url, deep) -> None:
+    def __init__(self, url:str, deep_limit:int) -> None:
         self.url = url
-        self.deep = deep
+        self.deep_limit = deep_limit
 
     def run(self):
-        html = self.get_html(self.url)
-        links = self.get_links(html)
-        def remove_repetitions():
-             return list(set(links))
-        # ! where is current url ?
-
-    def get_html(self, url) -> str:
-        response: Response = requests.get(url)
-        if response.status_code == 200:
-            return response.content
-    
-
-    def get_links(self, text_html,current_url):
-        tree: HtmlElement = html.fromstring(text_html)
-        expresion = "//body//a"
-        elements: list[HtmlElement] = tree.xpath(expresion)
-        
-        def normalise (url:str):
-            parse = urlsplit(url)
-            base_url = f"{parse.scheme}://{parse.netloc}"
-            normalise_links = [urljoin(base_url,e.attrib['href'])  for e in elements ]
-            return normalise_links
-            # normalise_links = []
-            # for e in elements:
-            #     link = e.attrib['href']
-            #     n_link = urljoin(base_url,link)
-            #     normalise_links.append(n_link)
-
-        return normalise(current_url)    
+        pass
             
 
-    def dfs(self, deep):
-        if deep < self.deep:
-            return
-    
-    def bfs(self, deep):
-        pass
