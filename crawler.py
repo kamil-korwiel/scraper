0cd480d0fe5dc969f89d5021603988d1ad2efad9
kamil-korwiel <kamilkorwiel@gmail.com>
2024-04-21 23:55:47 +0200

    Change a search funcion

--- crawler.py
+++ crawler.py
@@ -14,13 +14,13 @@ def get_html(url:str) -> str:
         return response.content
 
 
-def get_links(current_url:str):
+def get_links(current_url:str) -> list[str]:
     text_html = get_html(current_url)
     tree: HtmlElement = html.fromstring(text_html)
     expresion = "//body//a"
     elements: list[HtmlElement] = tree.xpath(expresion)
     
-    def normalise (url:str):
+    def normalise (url:str) -> list[str]:
         parse = urlsplit(url)
         base_url = f"{parse.scheme}://{parse.netloc}"
         normalise_links = [urljoin(base_url, e.attrib['href'])  for e in elements ]
@@ -36,16 +36,29 @@ def bfs(url: str, deep_limit: int):
 
 
 class Crawler:
-    url: str = ""
-    deep_limit: int = 2
+    start_url: str
+    deep_limit: int
     grap_url: dict[str : list[str]]
-    visited: set[str] = {}
+    url_in_process: set
 
-    def __init__(self, url:str, deep_limit:int) -> None:
-        self.url = url
+    def __init__(self, url:str, deep_limit:int=2) -> None:
+        self.start_url = url
         self.deep_limit = deep_limit
 
     def run(self):
         pass
             
-
+    def _get_by_bfs(self):
+        que:Queue[str] = Queue() #FIFO
+        que.put((self.start_url, 0))
+        while not que.empty():
+            current_url, current_deep = que.get()    
+            if current_deep > self.deep_limit:
+                break
+                
+            list_links = get_links(current_url)
+            ###############FILTER###################
+           
+            for link in list_links:
+                que.put(link, current_deep + 1)
+            
